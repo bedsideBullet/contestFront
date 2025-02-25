f1214dba1061fe4a09b4cdb3ff15f3db26b04d4f
bedsideBullet <danjsmith3@gmail.com>
2025-02-24 18:10:03 -0600

    admindash

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	define: {},
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	define: {},
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
